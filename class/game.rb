933c2f19ec81fde565af3478da61b3d1b78d5651
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-06-21 00:10:56 -0700

    Add randomQuestion to question class

--- class/game.rb
+++ class/game.rb
@@ -2,18 +2,19 @@ class Game
 
   def initialize(p1, p2)
     @players = [p1, p2]
-    @player_index = 0
-    @current_player = players[player_index]
+    @index = 0
+    @current_player = @players[@index]
   end
 
   def change_player
-    if current_player == player_index
+    if @current_player == players[player_index]
       @current_player = players[1]
     else
-      @current_player = 0 
+      @current_player = players[0] 
     end
   end
 
-  
 
 end
+
+
--- class/question.rb
+++ class/question.rb
@@ -4,13 +4,45 @@ class Question
     @answer = answer
   end
 
-  def random_question
-    num1 = rand(101)
-    num2 = rand(101)
+  # def random_question
+  #   num1 = rand(101)
+  #   num2 = rand(101)
     
-    @answer = num1 + num2
-    puts "What is #{num1} + #{num2} equal to"
-  end
+  #   @answer = num1 + num2
+  #   puts "What is #{num1} + #{num2} equal to?"
+
+  #   playerAnswer = $stdin.gets.chomp
+
+  #   if playerAnswer == @answer
+  #     puts "You are correct!"
+  #   else
+  #     puts "You are soo wrong!"
+  #   end
+  # end
   
 end
+
+def random_question
+  num1 = rand(101)
+  num2 = rand(101)
+  
+  answer = num1 + num2
+  puts "What is #{num1} + #{num2} equal to?"
+  
+  puts "TESTING ANSWER--------#{answer.class}"
+  puts "TESTING ANSWER--------#{answer}"
+
+  playerAnswer = $stdin.gets.chomp.to_i
+
+  puts "Testing PLAYER ANSWER------#{playerAnswer}"
+  puts "Testing PLAYER ANSWER------#{playerAnswer.class}"
+
+  if playerAnswer == answer
+    puts "You are correct!"
+  else
+    puts "You are soo wrong!"
+  end
+end
+
+random_question()
 
